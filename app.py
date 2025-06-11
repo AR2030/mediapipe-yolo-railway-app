9fa237f206b31b001d6677b75a2f3ec3d3dc3ba6
Abdelrhman Albarbary <53479996+AR2030@users.noreply.github.com>
2025-06-11 22:14:25 +0300

    Update app.py
    update mp_hands

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ try:
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
         static_image_mode=True,
-        max_num_hands=1,
-        min_detection_confidence=0.3
+        max_num_hands=2,
+        min_detection_confidence=0.1
     )
     mp_drawing = mp.solutions.drawing_utils
     print("✅ MediaPipe initialized successfully.")
